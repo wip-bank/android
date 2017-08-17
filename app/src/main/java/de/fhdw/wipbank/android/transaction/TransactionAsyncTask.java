67534738e9796c2f0c12f14bec000c01a62748a7
dsawenko <daniel.sawenko@web.de>
2017-08-17 21:33:48 +0200

    Kleine Änderung

--- app/src/main/java/de/fhdw/wipbank/android/transaction/TransactionAsyncTask.java
+++ app/src/main/java/de/fhdw/wipbank/android/transaction/TransactionAsyncTask.java
@@ -65,7 +65,7 @@ public class TransactionAsyncTask extends AsyncTask<Void, Void, HttpResponse> {
         }
 
 
-        url = String.format("http://%s:9998/rest/transaction", ip); //  "http://10.0.2.2:9998/rest/transaction" für Localhost / Daniels Laptop: 192.168.43.182:9998
+        url = String.format("http://%s/rest/transaction", ip); //  "http://10.0.2.2:9998/rest/transaction" für Localhost / Daniels Laptop: 192.168.43.182:9998
     }
 
     @Override
