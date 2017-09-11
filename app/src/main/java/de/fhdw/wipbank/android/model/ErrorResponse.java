e9cc6a8c1cde8068cf1ea68bf3ab1d79a30b4da7
dsawenko <daniel.sawenko@web.de>
2017-09-11 20:21:44 +0200

    Kommentare

--- app/src/main/java/de/fhdw/wipbank/android/model/ErrorResponse.java
+++ app/src/main/java/de/fhdw/wipbank/android/model/ErrorResponse.java
@@ -4,10 +4,20 @@ public class ErrorResponse {
 
     private String error;
 
+    /**
+     * Gibt die Fehlermeldung zurück.
+     *
+     * @return error
+     */
     public String getError() {
         return error;
     }
 
+    /**
+     * Setzt die Fehlermeldung.
+     *
+     * @param error
+     */
     public void setError(String error) {
         this.error = error;
     }
